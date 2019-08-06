e0b7009fd54e998ecb227fc165d6a577ce940225
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-06 01:00:15 +0100

    Knn turned into class

--- knn.py
+++ knn.py
@@ -1,23 +1,21 @@
 import numpy as np
 from collections import Counter
 
-class knn():
+class KNearestNeighbor():
     def __init__(self, *args, **kwargs):
         return super().__init__(*args, **kwargs)
-    
-    def predict(self, datum, sample, k=3):
-        distances = []
-        for group in datum:
-            for features in datum[group]:
-                l2norm = np.linalg.norm(np.array(features)-np.array(sample))
-                distances.append([l2norm, group])
 
-        votes = [i[1] for i in sorted(distances)[:k]]
+    def predict(self, data, sample, k=3):
+        distances = [[np.linalg.norm(np.array(features)-np.array(sample)), diagnosis]
+                     for diagnosis in data for features in data[diagnosis]]
+
+        votes = [di[1] for di in sorted(distances)[:k]]
         verdict = Counter(votes).most_common(1)[0][0]
 
-        return verdict 
+        return verdict
 
-dataset = {'A': [[1, 2], [2, 3], [3, 1]], 'B': [[6, 5], [7, 7], [8, 6]]}
-example = [5, 7]
-knn = knn()
-print(knn.predict(dataset, example, k=3))
+
+# dataset = {"A": [[1, 2], [2, 3], [3, 1]], "B": [[6, 5], [7, 7], [8, 6]]}
+# example = [5, 7]
+# knn = KNearestNeighbor()
+# print(knn.predict(dataset, example, k=3))
