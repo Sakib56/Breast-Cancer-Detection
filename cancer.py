487740c0e935a3e84b03576c0f12ac02547021cb
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-06 01:00:44 +0100

    data loaded into new format
    dictionary

--- cancer.py
+++ cancer.py
@@ -3,32 +3,27 @@ import knn
 
 def loadData():
     with open("data.csv") as dataCSV:
-        data = []
-        targets = []
+        data = {"B": [], "M": []}
         for row in dataCSV:
-            # removing all nonalphanumeric chars from dataCSV
             cleanedRow = list(filter(None, map(lambda x: x.strip(), row.split(","))))
-            data.append(cleanedRow)
-            if cleanedRow[1] == "M":
-                targets.append([0,1])
-            if cleanedRow[1] == "B":
-                targets.append([1,0])
+            diagnosis = cleanedRow[1]
+            cleanedRow = cleanedRow[2:]
 
-        dataMat = np.matrix(data)
-        dataTargets = np.matrix(targets)
+            if diagnosis == "B":
+                cleanedRow = list(map(lambda x: float(x), cleanedRow))
+                data["B"].append(cleanedRow)
+            if diagnosis == "M":
+                cleanedRow = list(map(lambda x: float(x), cleanedRow))
+                data["M"].append(cleanedRow)
 
-        dataMat = np.delete(dataMat, 0, axis=0)
-        dataTargets = np.delete(dataTargets, 0, axis=0)
-
-        dataMat = np.delete(dataMat, 0, axis=1)
-        dataMat = np.delete(dataMat, 0, axis=1)
-
-        return dataMat.astype(np.float), dataTargets.astype(np.int)
+        return data
 
 ### MAIN ###
-data, targets = loadData()
+dataset = loadData()
+knn = knn.KNearestNeighbor()
 
-Xtrain = data[0:284, :]
-Ytrain = targets[0:284, :]
-Xtest = data[285:569, :]
-Ytest = targets[285:569, :]
+diagnosis = "M"
+
+for i in range(len(dataset[diagnosis])):
+    example = dataset[diagnosis][i]
+    print(knn.predict(dataset, example, k=5))
